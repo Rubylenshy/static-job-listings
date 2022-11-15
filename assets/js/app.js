76e506fc6f96b1f5be254cbcb5d096a0fc47d643
Rubylenshy <reztomoloju@gmail.com>
2022-11-15 14:04:14 +0100

    filter feature updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,20 +21,26 @@ async function jobListings(file){
     jobList.forEach(item =>{ 
         let languageBtn = '';
         let lang_s = '';
+        for(let i = 0; i < item.languages.length; i++){
+            lang_s += `data-languages${i} = '${item.languages[i]}' `
+        }
         item.languages.forEach(lang =>{
             languageBtn += `<button type="button">${lang}</button>`
-            lang_s += `${lang} `
+            // lang_s += `${lang} `
         });
 
         let toolsBtn = '';
         let tool_s = '';
+        for (let i = 0; i < item.tools.length; i++) {
+            tool_s += `data-tools${i} = '${item.tools[i]}' `             
+        }
         item.tools.forEach(tool =>{
             toolsBtn += `<button type="button">${tool}</button>`
-            tool_s += `${tool} `
+            // tool_s += `${tool} `
         });
 
         let htmljobList = `<div class="job-list ${item.featured}" data-role="${item.role}" data-level="${item.level}" 
-                            data-languages="${lang_s}" data-tools="${tool_s}">
+                            ${lang_s} ${tool_s}>
                                 <div class="list-group">
                                 <img src="${item.logo}" alt="" width="50" height="50" >
                                 <div>
@@ -107,16 +113,26 @@ async function jobListings(file){
                         })
                         btnfilter.append(cross)
                         filterDiv.append(btnfilter)
+
+                        jobItem.forEach(job=>{
+                            let role = job.dataset.role
+                            let level = job.dataset.level
+                            let l0 = job.dataset.languages0
+                            let l1 = job.dataset.languages1
+                            let l2 = job.dataset.languages2
+                            let t0 = job.dataset.tools0
+                            let t1 = job.dataset.tools1
+                            if(role == btnfilter.textContent || level == btnfilter.textContent || l0 == btnfilter.textContent
+                                || l1 == btnfilter.textContent || l2 == btnfilter.textContent || t0 == btnfilter.textContent
+                                || t1 == btnfilter.textContent){
+                                job.style.display = 'block'
+                            }
+                            else{job.style.display = 'none'}
+                        })
                     }
                     filter()
-                    jobItem.forEach(job=>{
-                        if(job.dataset.role == 'Frontend'){
-                            job.style.display = 'none'
-                        }
-                        // else if(job.dataset.role == 'Backend'){
-                        //     job.style.display = 'none'
-                        // }
-                    })
+
+                    
                 })
             })
 
